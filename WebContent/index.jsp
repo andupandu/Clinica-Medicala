6d179362e5a995f07b79c7d31b43043d8ba24909
Andreea Limba <andreea.limba@yahoo.com>
2017-12-03 22:46:20 +0200

    Show all specializations on first page

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -1,10 +1,26 @@
+<%@page import="pkg.DbOperations"%>
+<%@page import="java.util.List"%>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<link rel="stylesheet" type="text/css" href="Styles/Style.css">
 <title>Clinica medicala</title>
 </head>
 <body>
 	<h1>Clinica Medicala!</h1>
+	<ul>
+ 
+  <li class="dropdown">
+    <a href="javascript:void(0)" class="dropbtn">Specializari</a>
+    <div class="dropdown-content">
+    <%List<String> specializari=DbOperations.getSpecializari(); 
+    for(String spec:specializari){%>
+      <a href="#"><%=spec %></a>
+<%} %>
+    </div>
+  </li>
+</ul>
+	
 	<form action="Hello">
 		<input type="submit" value="Press">
 	</form>
--- src/pkg/Hello.java
+++ src/pkg/Hello.java
@@ -1,7 +1,12 @@
 package pkg;
 
+import java.beans.Statement;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -29,7 +34,7 @@ public class Hello extends HttpServlet {
 		// TODO Auto-generated method stub
 	
 		PrintWriter msj=response.getWriter();
-		msj.print("Hello");
+		msj.print("Hello bb");
 	}
 
 	/**
@@ -38,6 +43,8 @@ public class Hello extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+	
 	}
-
 }
+
+
